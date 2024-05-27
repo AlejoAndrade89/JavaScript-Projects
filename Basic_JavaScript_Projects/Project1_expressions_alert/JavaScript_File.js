58879854acc803ead8043a716134e8175eb6948d
Alejandro Andrade <91246000+AlejoAndrade89@users.noreply.github.com>
2024-05-27 16:47:33 -0500

    Update JavaScript_File.js
    .

--- Basic_JavaScript_Projects/Project1_expressions_alert/JavaScript_File.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JavaScript_File.js
@@ -1,3 +1,15 @@
 var A = "This is a String";
-document.write(A);
+
+const firstName = "Daniel";
+const lastName = "Andrade"; //defining a vriable
+
+var B = "Breakfast",
+  C = "sausage",
+  D = "eggs",
+  E = "Bacon";
+
+var fullBreakfast = D + C + E;
+
+document.write(D);
+document.write(firstName + " " + lastName);
 window.alert(A);
